a4f9773f391faf56e3b69d1fcb6e735215645d11
Lhuana Lee <leelhuana@gmail.com>
2022-12-29 08:51:49 -0300

    source

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Login from './pages/Login/Login';
+
+import GlobalStyle from './styles/globalStyle';
+
+const App = () => {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Login />
+    </div>
+  );
+};
+
+export default App;
