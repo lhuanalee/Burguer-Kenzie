34d870a39193434b2f5f8c72f3b09386187ba69d
Lhuana Lee <leelhuana@gmail.com>
2022-12-30 11:06:56 -0300

    feat: Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,16 @@
-const Button = () => {
+import { ReactNode } from "react";
+
+import { ButtonStyle } from "./ButtonStyle";
+
+interface iButtonProps {
+    children: ReactNode;
+    className: string,
+}
+
+const Button = ({ className, children }: iButtonProps) => {
     return (
-        <button></button>
+        <ButtonStyle className={className}>{children}</ButtonStyle>
     );
 };
+
+export default Button;
---
+++ src/components/Button/ButtonStyle.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ButtonStyle = styled.button`
+    padding: 17px 0;
+    
+    border: 1px solid transparent;
+    border-radius: 8px;
+
+    font-size: var(--headline-size);
+    font-weight: var(--body-weight-2);
+
+    color: var(--white);
+    background-color: var(--color-brad);
+
+    cursor: pointer;
+`
