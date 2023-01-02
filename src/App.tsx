31b231493640ece2ba36d35f27f13bf33ea502d4
Lhuana Lee <leelhuana@gmail.com>
2023-01-02 01:44:02 -0300

    source

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import Router from './routes/Router';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
-import GlobalStyle from './styles/globalStyle';
+import Router from "./routes/Router";
+
+import GlobalStyle from "./styles/globalStyle";
 
 const App = () => {
   return (
     <div className="App">
       <GlobalStyle />
+      <ToastContainer theme="dark" />
       <Router />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
+import App from "./App";
+import CartProvider from "./contexts/CartContext/CartContext";
+import ProductProvider from "./contexts/ProductContext/ProductContext";
+import UserProvider from "./contexts/UserContext/UserContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <ProductProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
