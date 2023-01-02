8bfcc0db730d952884beba39af6e0f34d0c74bc9
Lhuana Lee <leelhuana@gmail.com>
2023-01-02 01:52:36 -0300

    refactor: Dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+import Cart from "../../components/Dashboard/Cart/Cart";
+import Header from "../../components/Dashboard/Header/Header";
+import ProductsList from "../../components/Dashboard/ProductList/ProductList";
+import { CartContext } from "../../contexts/CartContext/CartContext";
+
+import { DashboardStyle } from "./DashboardStyle";
+
 const Dashboard = () => {
-    return (
-        <div></div>
-    );
+  const { click } = useContext(CartContext);
+
+  return (
+    <DashboardStyle>
+      <Header />
+      {click ? <Cart /> : <ProductsList />}
+    </DashboardStyle>
+  );
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/DashboardStyle.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const DashboardStyle = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  position: relative;
+`;
