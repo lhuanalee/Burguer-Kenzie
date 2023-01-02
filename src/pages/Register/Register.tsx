230f5b9040a81a3e8c71969b58d35db582521dfd
Lhuana Lee <leelhuana@gmail.com>
2023-01-02 01:52:16 -0300

    refactor: Register

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,35 +1,56 @@
+import { useContext } from "react";
+
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { UserContext } from "../../contexts/UserContext/UserContext";
+import { iRegisterData } from "../../contexts/UserContext/types";
+
+import { formSchemaRegister } from "../../configs/FormSchemaRegister/FormSchemaRegister";
 
 import Logo from "../../components/Logo/Logo";
-
-import Ellipse from "../../assets/Ellipse.png"
-import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+
+import Ellipse from "../../assets/Ellipse.png";
 
 import { RegisterStyle } from "./RegisterStyle";
 
 const Register = () => {
-    return (
-        <RegisterStyle>
-            <div className="banner">
-                <Logo />
-                <img src={Ellipse} className="ellipse" alt="Bolinhas" />
-            </div>
-            <div className="form">
-                <form>
-                    <div className="registerHeader">
-                        <h4>Cadastro</h4>
-                        <Link to="/">Retornar para o login</Link>
-                    </div>
-                    <Input type="text" children="Nome"/>
-                    <Input type="email" children="Email"/>
-                    <Input type="password" children="Senha"/>
-                    <Input type="password" children="Confirmar senha"/>
-                    <Button className="register">Cadastrar</Button>
-                </form>
-            </div>
-        </RegisterStyle>
-    );
+  const { userRegistration } = useContext(UserContext);
+
+  const { register, handleSubmit } = useForm<iRegisterData>({
+    resolver: yupResolver(formSchemaRegister),
+  });
+
+  return (
+    <RegisterStyle>
+      <div className="banner">
+        <Logo />
+        <img src={Ellipse} className="ellipse" alt="Bolinhas" />
+      </div>
+      <div className="form">
+        <form onSubmit={handleSubmit(userRegistration)}>
+          <div className="registerHeader">
+            <h4>Cadastro</h4>
+            <Link to="/">Retornar para o login</Link>
+          </div>
+          <Input type="text" label="Nome" {...register("name")} />
+          <Input type="email" label="E-mail" {...register("email")} />
+          <Input type="password" label="Senha" {...register("password")} />
+          <Input
+            type="password"
+            label="Confirmar senha"
+            {...register("confirmPassword")}
+          />
+          <Button type="submit" className="register">
+            Cadastrar
+          </Button>
+        </form>
+      </div>
+    </RegisterStyle>
+  );
 };
 
 export default Register;
--- src/pages/Register/RegisterStyle.ts
+++ src/pages/Register/RegisterStyle.ts
@@ -1,97 +1,96 @@
 import styled from "styled-components";
 
 export const RegisterStyle = styled.div`
-    max-width: 100vw;
-    min-height: 100vh;
+  max-width: 100vw;
+  min-height: 100vh;
 
-    padding: 40px 18px;
+  padding: 40px 18px;
 
-    display: flex;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .banner {
+    display: none;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    gap: 30px;
 
-    .banner {
-        display: none;
-        flex-direction: column;
-        gap: 30px;
-
-        .ellipse {
-            width: 50%;
-        }
+    .ellipse {
+      width: 50%;
     }
+  }
 
-    .form {
+  .form {
+    width: 100%;
+
+    margin-bottom: 40px;
+
+    border: 2px solid var(--grey-1);
+    border-radius: 5px;
+    box-shadow: 0px 0px 30px -20px rgba(0, 0, 0, 0.25);
+
+    background-color: var(--white);
+
+    form {
+      width: 100%;
+
+      padding: 18px;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 20px;
+
+      .registerHeader {
         width: 100%;
 
-        margin-bottom: 40px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
 
-        border: 2px solid var(--grey-1);
-        border-radius: 5px;
-        box-shadow: 0px 0px 30px -20px rgba(0, 0, 0, 0.25);
+        h4 {
+          width: 0;
 
-        background-color: var(--white);
+          font-size: var(--title-size-3);
+          font-weight: var(--headline-weight);
 
-        form {
-            width: 100%;
-    
-            padding: 18px;
-    
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            gap: 20px;
-
-            text-align: center;
-
-            .registerHeader {
-                width: 100%;
-
-                display: flex;
-                justify-content: space-between;
-
-                h4 {
-                    width: 0;
-
-                    font-size: var(--title-size-3);
-                    font-weight: var(--headline-weight);
-
-                    color: var(--color-brad);
-                }
-
-                a {
-                    font-size: var(--body-size-1);
-                    font-weight: var(--body-weight-1);
-
-                    text-decoration: underline;
-
-                    color: var(--grey-3);
-                }
-            }
-
-            button {
-                width: 100%;
-            }
+          color: var(--color-brad);
         }
-    }
 
-@media (min-width: 540px) {
+        a {
+          font-size: var(--body-size-1);
+          font-weight: var(--body-weight-1);
+
+          text-decoration: underline;
+
+          color: var(--grey-3);
+        }
+      }
+
+      button {
+        width: 100%;
+      }
+    }
+  }
+
+  @media (min-width: 540px) {
     .form {
-        width: 500px;
+      width: 500px;
     }
-}
+  }
 
-@media (min-width: 768px) {
+  @media (min-width: 768px) {
     flex-direction: row;
     gap: 62px;
 
-    .banner {   
-        display: flex;
+    .banner {
+      display: flex;
     }
 
     .form {
-        margin-bottom: 0;
+      margin-bottom: 0;
     }
-}
-`
+  }
+`;
