4f855f9b3a1a1783d2ec5784f9b4197e00bfb38a
Lhuana Lee <leelhuana@gmail.com>
2023-01-02 01:47:04 -0300

    style: Button

--- src/components/Button/ButtonStyle.ts
+++ src/components/Button/ButtonStyle.ts
@@ -1,22 +1,22 @@
 import styled from "styled-components";
 
 export const ButtonStyle = styled.button`
-    padding: 17px 0;
-    
-    border: 1px solid transparent;
-    border-radius: 8px;
+  padding: 17px 0;
 
-    font-size: var(--headline-size);
-    font-weight: var(--body-weight-2);
+  border: 1px solid transparent;
+  border-radius: 8px;
 
-    color: var(--white);
-    background-color: var(--color-brad);
+  font-size: var(--headline-size);
+  font-weight: var(--body-weight-2);
 
-    cursor: pointer;
+  color: var(--white);
+  background-color: var(--color-brad);
 
-    &:hover {
-        background-color: var(--color-primary);
+  cursor: pointer;
 
-        transition: 1s;
-    }
-`
+  &:hover {
+    background-color: var(--color-primary);
+
+    transition: 1s;
+  }
+`;
