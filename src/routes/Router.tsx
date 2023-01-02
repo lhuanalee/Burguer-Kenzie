9c19c07a99de5ac2c0e0dc3aa534817f8b826fde
Lhuana Lee <leelhuana@gmail.com>
2023-01-02 02:01:38 -0300

    feat: Router

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,15 +1,17 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 
-import Login from "../pages/Login/Login"
-import Register from "../pages/Register/Register"
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Dashboard from "../pages/Dashboard/Dashboard";
 
 const Router = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 };
 
 export default Router;
