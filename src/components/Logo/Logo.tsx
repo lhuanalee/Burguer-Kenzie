0a0f28ae7467a4624bcd5931fb27baaf27ad1d8e
Lhuana Lee <leelhuana@gmail.com>
2023-01-02 01:51:10 -0300

    refactor: Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,19 +1,23 @@
 import Bag from "../../assets/Bag.png";
 import BurguerKenzie from "../../assets/Burguer Kenzie.svg";
+
 import { LogoStyle } from "./LogoStyle";
 
 const Logo = () => {
-    return (
-        <LogoStyle>
-            <img src={BurguerKenzie} alt="Logo Burguer Kenzie" />
-            <div className="slogan">
-                <figure>
-                    <img src={Bag} alt="Sacola" />
-                </figure>
-                <p>A vida é como um sanduíche, é preciso recheá-la com os <span>melhores</span> ingredientes.</p>
-            </div>
-        </LogoStyle>
-    );
+  return (
+    <LogoStyle>
+      <img src={BurguerKenzie} alt="Logo Burguer Kenzie" />
+      <div className="slogan">
+        <figure>
+          <img src={Bag} alt="Sacola" />
+        </figure>
+        <p>
+          A vida é como um sanduíche, é preciso recheá-la com os{" "}
+          <span>melhores</span> ingredientes.
+        </p>
+      </div>
+    </LogoStyle>
+  );
 };
 
 export default Logo;
