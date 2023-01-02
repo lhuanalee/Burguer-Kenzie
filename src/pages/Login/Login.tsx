c22982415221ebc7fb723d229858252f9ba98779
Lhuana Lee <leelhuana@gmail.com>
2023-01-02 01:51:50 -0300

    refactor: Login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,31 +1,53 @@
+import { useContext } from "react";
+
+import { LoginStyle } from "./LoginStyle";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { iLoginData } from "../../contexts/UserContext/types";
+import { UserContext } from "../../contexts/UserContext/UserContext";
+
+import { formSchemaLogin } from "../../configs/FormSchemaLogin/FormSchemaLogin";
+
+import Input from "../../components/Input/Input";
+import Anchor from "../../components/Anchor/Anchor";
+
 import Logo from "../../components/Logo/Logo";
 import Ellipse from "../../assets/Ellipse.png";
 
-import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
-import Anchor from "../../components/Anchor/Anchor";
-
-import { LoginStyle } from "./LoginStyle";
 
 const Login = () => {
-    return (
-        <LoginStyle>
-            <div className="banner">
-                <Logo />
-                <img src={Ellipse} className="ellipse" alt="Bolinhas" />
-            </div>
-            <div className="form">
-                <form>
-                    <h4>Login</h4>
-                    <Input type="text" children="Nome"/>
-                    <Input type="text" children="Senha"/>
-                    <Button className="login">Logar</Button>
-                    <span>Crie sua conta para saborear muitas delícias e matar sua fome!</span>
-                    <Anchor to="/register" className="register">Cadastrar</Anchor>
-                </form>
-            </div>
-        </LoginStyle>
-    );
+  const { userLogin } = useContext(UserContext);
+
+  const { register, handleSubmit } = useForm<iLoginData>({
+    resolver: yupResolver(formSchemaLogin),
+  });
+
+  return (
+    <LoginStyle>
+      <div className="banner">
+        <Logo />
+        <img src={Ellipse} className="ellipse" alt="Bolinhas" />
+      </div>
+      <div className="form">
+        <form onSubmit={handleSubmit(userLogin)}>
+          <h4>Login</h4>
+          <Input type="email" label="E-mail" {...register("email")} />
+          <Input type="password" label="Senha" {...register("password")} />
+          <Button type="submit" className="login">
+            Logar
+          </Button>
+          <span>
+            Crie sua conta para saborear muitas delícias e matar sua fome!
+          </span>
+          <Anchor to="/register" className="register">
+            Cadastrar
+          </Anchor>
+        </form>
+      </div>
+    </LoginStyle>
+  );
 };
 
 export default Login;
--- src/pages/Login/LoginStyle.ts
+++ src/pages/Login/LoginStyle.ts
@@ -1,113 +1,113 @@
 import styled from "styled-components";
 
 export const LoginStyle = styled.div`
-    max-width: 100vw;
-    min-height: 100vh;
+  max-width: 100vw;
+  min-height: 100vh;
 
-    padding: 40px 18px;
+  padding: 40px 18px;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 14px;
+
+  .banner {
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 14px;
+    gap: 30px;
 
-    .banner {
-        display: flex;
-        flex-direction: column;
-        gap: 30px;
+    .ellipse {
+      width: 50%;
 
-        .ellipse {
-            width: 50%;
-
-            display: none;
-        }
+      display: none;
     }
+  }
 
-    .form {
+  .form {
+    width: 100%;
+
+    margin-bottom: 40px;
+
+    border: 2px solid var(--grey-1);
+    border-radius: 5px;
+    box-shadow: 0px 0px 30px -20px rgba(0, 0, 0, 0.25);
+
+    background-color: var(--white);
+
+    form {
+      width: 100%;
+
+      padding: 18px;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 20px;
+
+      text-align: center;
+
+      h4 {
         width: 100%;
 
-        margin-bottom: 40px;
+        font-size: var(--title-size-3);
+        font-weight: var(--headline-weight);
 
-        border: 2px solid var(--grey-1);
-        border-radius: 5px;
-        box-shadow: 0px 0px 30px -20px rgba(0, 0, 0, 0.25);
+        color: var(--color-brad);
 
-        background-color: var(--white);
+        text-align: left;
+      }
 
-        form {
-            width: 100%;
-    
-            padding: 18px;
-    
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            gap: 20px;
+      .login {
+        width: 100%;
+      }
 
-            text-align: center;
-    
-            h4 {
-                width: 100%;
+      span {
+        width: 70%;
 
-                font-size: var(--title-size-3);
-                font-weight: var(--headline-weight);
+        font-size: var(--body-size-1);
+        font-weight: var(--caption-weight);
 
-                color: var(--color-brad);
-    
-                text-align: left;
-            }        
-    
-            .login {
-                width: 100%;
-            }
+        color: var(--grey-5);
+      }
 
-            span {
-                width: 70%;
+      .register {
+        width: 100%;
 
-                font-size: var(--body-size-1);
-                font-weight: var(--caption-weight);
+        color: var(--grey-5);
+        background-color: var(--grey-1);
 
-                color: var(--grey-5);
-            }
+        &:hover {
+          background-color: var();
 
-            .register {
-                width: 100%;
+          color: var(--grey-1);
+          background-color: var(--grey-5);
 
-                color: var(--grey-5);
-                background-color: var(--grey-1);
-
-                &:hover {
-                    background-color: var();
-
-                    color: var(--grey-1);
-                    background-color: var(--grey-5);
-
-                    transition: 1s;
-                }
-            }
+          transition: 1s;
         }
+      }
     }
+  }
 
-@media (min-width: 540px) {
+  @media (min-width: 540px) {
     .form {
-        width: 500px;
+      width: 500px;
     }
-}
+  }
 
-@media (min-width: 768px) {
+  @media (min-width: 768px) {
     flex-direction: row-reverse;
     gap: 62px;
 
     .banner {
-        .ellipse {
-            display: flex;
-        }
+      .ellipse {
+        display: flex;
+      }
     }
 
     .form {
-        margin-bottom: 0;
+      margin-bottom: 0;
     }
-}
-`
+  }
+`;
